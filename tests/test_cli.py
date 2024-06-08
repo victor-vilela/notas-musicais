db1f89cf1370b136f280e5529aeb1d848c53bbd1
Victor Vilela <victor_vilel@hotmail.com>
2024-06-08 11:55:04 -0300

    Implementação do comando de acorde no CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,23 +7,35 @@ runner = CliRunner()
 
 
 def test_escala_cli_deve_retornar_0_stdout():
-    result = runner.invoke(app)
+    result = runner.invoke(app, ['escala'])
     assert result.exit_code == 0
 
 
 @mark.parametrize('nota', ['C', 'D', 'E', 'F', 'G', 'A', 'B'])
 def test_escala_cli_deve_conter_notas_resposta(nota):
-    result = runner.invoke(app)
+    result = runner.invoke(app, ['escala'])
     assert nota in result.stdout
 
 
 @mark.parametrize('nota', ['F', 'G', 'A', 'A#', 'C', 'D', 'E'])
 def test_escala_cli_deve_conter_notas_resposta_fa(nota):
-    result = runner.invoke(app, ['F'])
+    result = runner.invoke(app, ['escala', 'F'])
     assert nota in result.stdout
 
 
 @mark.parametrize('graus', ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'])
 def test_escala_cli_deve_conter_todos_graus(graus):
-    result = runner.invoke(app, ['F'])
+    result = runner.invoke(app, ['escala', 'F'])
     assert graus in result.stdout
+
+
+@mark.parametrize('nota', ['C', 'E', 'G'])
+def test_acorde_cli_deve_conter_notas_resposta(nota):
+    result = runner.invoke(app, ['acorde'])
+    assert nota in result.stdout
+
+
+@mark.parametrize('grau', ['I', 'III', 'V'])
+def test_acorde_cli_deve_conter_notas_resposta(grau):
+    result = runner.invoke(app, ['escala', 'F'])
+    assert grau in result.stdout
